f756581473b78608602c71a802a3d5ea754f00c2
Romain Martinez <romain.martinezquey@MacBook-Pro-2.local>
2025-02-11 13:53:15 +0100

    feat: implémentation du composant Header

---
+++ header-mfe/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ background: '#f0f0f0', padding: '1rem' }}>
+      <h2>Header Micro Frontend</h2>
+    </header>
+  );
+};
+
+export default Header; 
